4336b919d89fae5d99723e222c419cd56d0161f7
takaodaze <gin2798@gmail.com>
2026-03-27 23:44:01 +0900

    コマンド追加

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -13,6 +13,15 @@ export const commandSets: CommandSet[] = [
 	{ command: 'カース > 両手 > スケゴ > 片手 > 集中', durationMs: DEFAULT_DURATION },
 	{ command: 'カース > 両手 > スケゴ > 集中', durationMs: DEFAULT_DURATION },
 
+	{ command: 'カース > 両手 > 集中', durationMs: DEFAULT_DURATION },
+
+	{ command: 'カース > 片手 > 両手 > 集中', durationMs: DEFAULT_DURATION },
+	{ command: 'カース > 片手 > スケゴ > 片手 > 集中', durationMs: DEFAULT_DURATION },
+	{ command: 'カース > 片手 > スケゴ > 片手 > 両手 > 集中', durationMs: DEFAULT_DURATION },
+	{ command: 'カース > 片手 > スケゴ > 両手 > 集中', durationMs: DEFAULT_DURATION },
+
+	{ command: 'カース > 片手 > 両手 > スケゴ > 集中', durationMs: DEFAULT_DURATION },
+
 	{ command: '両手 > スケゴ > 片手 > 両手 > 集中', durationMs: DEFAULT_DURATION },
 	{ command: '両手 > スケゴ > 両手 > 集中', durationMs: DEFAULT_DURATION },
 	{ command: '両手 > スケゴ > 片手 > 集中', durationMs: DEFAULT_DURATION },
@@ -23,18 +32,18 @@ export const commandSets: CommandSet[] = [
 	{ command: '両手 > スケゴ > カース > 片手 > 集中', durationMs: DEFAULT_DURATION },
 	{ command: '両手 > スケゴ > カース > 集中', durationMs: DEFAULT_DURATION },
 
-	{ command: '両手 > カース > 両手 > 片手 > 集中', durationMs: DEFAULT_DURATION },
+	{ command: '両手 > カース > 片手 > 両手 > 集中', durationMs: DEFAULT_DURATION },
 	{ command: '両手 > カース > 片手 > 集中', durationMs: DEFAULT_DURATION },
 	{ command: '両手 > カース > 両手 > 集中', durationMs: DEFAULT_DURATION },
-	{ command: 'カース > 片手 > 両手 > 集中', durationMs: DEFAULT_DURATION },
-	{ command: 'カース > 両手 > 集中', durationMs: DEFAULT_DURATION },
 	// 接近択
+	{ command: 'カース > 片手接近 > 前転 > 片手 > 近S', durationMs: DEFAULT_DURATION },
 	{ command: 'カース > 片手接近 > 前転 > 片手 > 近S', durationMs: DEFAULT_DURATION },
 	{ command: 'カース > 片手接近 > 近S', durationMs: DEFAULT_DURATION },
 	{ command: 'カース > Jダッシュ > (表)片手射撃 > 近S', durationMs: DEFAULT_DURATION },
 	{ command: 'カース > Jダッシュ > (裏)片手射撃固め > 近S', durationMs: DEFAULT_DURATION },
 	{ command: 'カース > Jダッシュ > (表)S固め > 近S', durationMs: DEFAULT_DURATION },
 	{ command: 'カース > Jダッシュ > (裏)D固め > 近S', durationMs: DEFAULT_DURATION },
+	{ command: 'カース > 両手 > 両手解除 > 片手接近 > 前転 > 近S', durationMs: DEFAULT_DURATION },
 	{ command: 'カース > 両手 > 両手解除 > 片手接近 > 前転 > 近S', durationMs: DEFAULT_DURATION }
 ];
 
